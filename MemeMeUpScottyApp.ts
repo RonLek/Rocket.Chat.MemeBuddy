7e3ce8f91f08196e55f43c617a38007261269b65
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-03-25 12:50:57 +0530

    Initial Commit

---
+++ MemeMeUpScottyApp.ts
@@ -0,0 +1,19 @@
+import {
+    IAppAccessors,
+    ILogger,
+    IConfigurationExtend,
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+
+import {ScottyCommand} from './commands/scotty'
+
+export class MemeMeUpScottyApp extends App {
+    constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
+        super(info, logger, accessors);
+    }
+
+    protected async extendConfiguration(configuration: IConfigurationExtend): Promise<void> {
+        await configuration.slashCommands.provideSlashCommand(new ScottyCommand(this));
+    }
+}
