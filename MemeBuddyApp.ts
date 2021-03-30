a0c46a36636a959d7a6fcf115043b31e618e6140
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-03-30 12:56:25 +0530

    [NEW] Provides meme subreddits to choose from

--- MemeBuddyApp.ts
+++ MemeBuddyApp.ts
@@ -1,19 +1,71 @@
 import {
     IAppAccessors,
-    ILogger,
     IConfigurationExtend,
-} from '@rocket.chat/apps-engine/definition/accessors';
-import { App } from '@rocket.chat/apps-engine/definition/App';
-import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+    IHttp,
+    ILogger,
+    IModify,
+    IPersistence,
+    IRead,
+} from "@rocket.chat/apps-engine/definition/accessors";
+import { App } from "@rocket.chat/apps-engine/definition/App";
+import { IAppInfo } from "@rocket.chat/apps-engine/definition/metadata";
+import { UIKitBlockInteractionContext } from "@rocket.chat/apps-engine/definition/uikit";
+import { ImageAttachment } from "./lib/ImageAttachment";
 
-import {MemeCommand} from './commands/meme'
+import { MemeCommand } from "./commands/meme";
 
 export class MemeBuddyApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
     }
 
-    protected async extendConfiguration(configuration: IConfigurationExtend): Promise<void> {
-        await configuration.slashCommands.provideSlashCommand(new MemeCommand(this));
+    public async executeBlockActionHandler(
+        context: UIKitBlockInteractionContext,
+        http: IHttp,
+        modify: IModify
+    ) {
+        const data = context.getInteractionData();
+        console.log("Data =", data);
+
+        const { actionId } = data;
+
+        switch (actionId) {
+            case "memeSelect": {
+                const memeResponse = await http.get(
+                    `https://meme-api.herokuapp.com/gimme/${data.value}/1`
+                );
+
+                const { room } = context.getInteractionData();
+
+                const builder = await modify
+                    .getCreator()
+                    .startMessage()
+                    .addAttachment(
+                        new ImageAttachment(memeResponse.data.memes[0].url)
+                    );
+
+                if (room) {
+                    builder.setRoom(room);
+                }
+
+                await modify.getCreator().finish(builder);
+
+                return {
+                    success: true,
+                };
+            }
+        }
+
+        return {
+            success: false,
+        };
+    }
+
+    protected async extendConfiguration(
+        configuration: IConfigurationExtend
+    ): Promise<void> {
+        await configuration.slashCommands.provideSlashCommand(
+            new MemeCommand(this)
+        );
     }
 }
--- commands/meme.ts
+++ commands/meme.ts
@@ -1,29 +1,80 @@
-import {ISlashCommand, SlashCommandContext} from '@rocket.chat/apps-engine/definition/slashcommands';
-import {IHttp, IModify, IRead} from '@rocket.chat/apps-engine/definition/accessors';
-import {App} from '@rocket.chat/apps-engine/definition/App';
-import {ImageAttachment} from '../lib/ImageAttachment'
+import { ButtonStyle } from "@rocket.chat/apps-engine/definition/uikit";
+
+import {
+    ISlashCommand,
+    SlashCommandContext,
+} from "@rocket.chat/apps-engine/definition/slashcommands";
+import {
+    IHttp,
+    IModify,
+    IRead,
+} from "@rocket.chat/apps-engine/definition/accessors";
+import { App } from "@rocket.chat/apps-engine/definition/App";
 
 export class MemeCommand implements ISlashCommand {
-    public command = 'meme';
-    public i18nDescription = 'Fetches a meme from r/ProgrammerHumor on Reddit';
-    public i18nParamsExample = '';
+    public command = "meme";
+    public i18nDescription = "Fetches a meme from r/ProgrammerHumor on Reddit";
+    public i18nParamsExample = "";
     public providesPreview = false;
 
     constructor(private readonly app: App) {}
 
-    public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp): Promise<void> {
-
-        const memeResponse = await http.get("https://meme-api.herokuapp.com/gimme/ProgrammerHumor/1")
-
-        const messageStructure = await modify.getCreator().startMessage();
+    public async executor(
+        context: SlashCommandContext,
+        read: IRead,
+        modify: IModify,
+        http: IHttp
+    ): Promise<void> {
         const sender = context.getSender(); // the user calling the slashcommand
         const room = context.getRoom(); // the current room
 
-        messageStructure
-        .setRoom(room)
-        .addAttachment(new ImageAttachment(memeResponse.data.memes[0].url))
-        .setText(`Here's a meme by _${sender.username}_ to cheer you up! :tada:`)
+        const greetBuilder = await modify
+            .getCreator()
+            .startMessage()
+            .setRoom(room)
+            .setText(`Hey _${sender.username}_ !`);
 
-        await modify.getCreator().finish(messageStructure);
+        await modify.getCreator().finish(greetBuilder);
+
+        const builder = await modify
+            .getCreator()
+            .startMessage()
+            .setRoom(room)
+            .setText(`Hey _${sender.username}_ !`);
+
+        const block = modify.getCreator().getBlockBuilder();
+        // TODO: Make options disappear when one selected
+
+        block.addSectionBlock({
+            text: block.newPlainTextObject("Choose a meme subreddit below 👇 "),
+        });
+
+        block.addActionsBlock({
+            blockId: "subreddits",
+            elements: [
+                block.newButtonElement({
+                    actionId: "memeSelect",
+                    text: block.newPlainTextObject("ProgrammerHumor"),
+                    value: "programmerhumor",
+                    style: ButtonStyle.PRIMARY,
+                }),
+                block.newButtonElement({
+                    actionId: "memeSelect",
+                    text: block.newPlainTextObject("DankMemes"),
+                    value: "dankmemes",
+                    style: ButtonStyle.PRIMARY,
+                }),
+                block.newButtonElement({
+                    actionId: "memeSelect",
+                    text: block.newPlainTextObject("WholesomeMemes"),
+                    value: "wholesomememes",
+                    style: ButtonStyle.PRIMARY,
+                }),
+            ],
+        });
+
+        builder.setBlocks(block);
+
+        await modify.getCreator().finish(builder);
     }
 }
